52c52961a19d05a93298b40edf5bc02a9af160b8
FelixChenJF <chenjingfeng202209@163.com>
2024-05-25 11:04:53 +0100

    form.py

---
+++ CHEN/courses/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class CustomForm(forms.Form):
+    audio1 = forms.FileField(label='Chinese Audio', required=False)
+    text1 = forms.CharField(label='Chinese Text', widget=forms.Textarea, required=False)
+    audio2 = forms.FileField(label='English Audio', required=False)
+    text2 = forms.CharField(label='English Text', widget=forms.Textarea, required=False)
+    message = forms.CharField(label='Comments', widget=forms.Textarea, required=False)
