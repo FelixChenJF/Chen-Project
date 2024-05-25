de8201932f18c93f2512a77700b2ad69ea3113d0
FelixChenJF <chenjingfeng202209@163.com>
2024-05-25 13:42:48 +0100

    存在很多问题

--- CHEN/courses/views.py
+++ CHEN/courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .forms import CustomForm
 from .models import Course, CustomData, Title, Label
 
@@ -52,42 +52,57 @@ def daily_vocabulary(request):
 
 
 def custom_page(request):
+    label_name = request.GET.get('label', None)
+    custom_data = None
+    
+    if label_name:
+        try:
+            label = Label.objects.get(label=label_name)
+            custom_data = CustomData.objects.filter(label=label).first()
+        except Label.DoesNotExist:
+            pass
+
     if request.method == 'POST':
         form = CustomForm(request.POST, request.FILES)
         if form.is_valid():
-            title_name = request.POST.get('title', '')
-            title, created = Title.objects.get_or_create(title=title_name)
-            
-            label_name = request.POST.get('label', '')
-            label = Label(label=label_name, title=title)
-            label.save()
-            
-            custom_data = CustomData(
-                audio1=form.cleaned_data['audio1'],
-                text1=form.cleaned_data['text1'],
-                audio2=form.cleaned_data['audio2'],
-                text2=form.cleaned_data['text2'],
-                message=form.cleaned_data['message'],
-                label=label
-            )
-            custom_data.save()
-            return redirect('/courses/detailedCourse/custom_page/')
-    else:
-        custom_data = CustomData.objects.all().first()
-        if custom_data:
-            initial_text1 = custom_data.text1
-            initial_text2 = custom_data.text2
-            initial_message = custom_data.message
+            # Process the form data and save it to the database
+            if custom_data:
+                custom_data.text1 = form.cleaned_data['text1']
+                custom_data.text2 = form.cleaned_data['text2']
+                custom_data.message = form.cleaned_data['message']
+                if 'audio1' in request.FILES:
+                    custom_data.audio1 = request.FILES['audio1']
+                if 'audio2' in request.FILES:
+                    custom_data.audio2 = request.FILES['audio2']
+                custom_data.save()
+            else:
+                # Create a new CustomData object if it doesn't exist
+                label = Label.objects.get_or_create(label=label_name)[0]
+                CustomData.objects.create(
+                    label=label,
+                    text1=form.cleaned_data['text1'],
+                    text2=form.cleaned_data['text2'],
+                    message=form.cleaned_data['message'],
+                    audio1=request.FILES.get('audio1', None),
+                    audio2=request.FILES.get('audio2', None)
+                )
+            # Return a JSON response indicating success
+            return JsonResponse({'success': True})
         else:
-            initial_text1 = ''
-            initial_text2 = ''
-            initial_message = ''
-
-        form = CustomForm()
+            # Return a JSON response indicating form errors
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        # Prepare initial form data
+        initial_data = {
+            'text1': custom_data.text1 if custom_data else '',
+            'text2': custom_data.text2 if custom_data else '',
+            'message': custom_data.message if custom_data else ''
+        }
+        form = CustomForm(initial=initial_data)
 
     return render(request, 'courses/detailedCourse/custom_page.html', {
         'form': form,
-        'initial_text1': initial_text1,
-        'initial_text2': initial_text2,
-        'initial_message': initial_message
+        'initial_text1': initial_data['text1'],
+        'initial_text2': initial_data['text2'],
+        'initial_message': initial_data['message']
     })
