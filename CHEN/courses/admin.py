4e9aacb5913e8ef224eb65cfd8a728d167849cad
FelixChenJF <chenjingfeng202209@163.com>
2024-05-25 14:51:22 +0100

    w

--- CHEN/courses/admin.py
+++ CHEN/courses/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course, Title, Label
+from .models import Course, Title, Label, CustomData
 
 admin.site.register(Course)
 admin.site.register(Title)
 admin.site.register(Label)
+admin.site.register(CustomData)
--- CHEN/courses/views.py
+++ CHEN/courses/views.py
@@ -52,57 +52,9 @@ def daily_vocabulary(request):
 
 
 def custom_page(request):
-    label_name = request.GET.get('label', None)
-    custom_data = None
-    
-    if label_name:
-        try:
-            label = Label.objects.get(label=label_name)
-            custom_data = CustomData.objects.filter(label=label).first()
-        except Label.DoesNotExist:
-            pass
-
-    if request.method == 'POST':
-        form = CustomForm(request.POST, request.FILES)
-        if form.is_valid():
-            # Process the form data and save it to the database
-            if custom_data:
-                custom_data.text1 = form.cleaned_data['text1']
-                custom_data.text2 = form.cleaned_data['text2']
-                custom_data.message = form.cleaned_data['message']
-                if 'audio1' in request.FILES:
-                    custom_data.audio1 = request.FILES['audio1']
-                if 'audio2' in request.FILES:
-                    custom_data.audio2 = request.FILES['audio2']
-                custom_data.save()
-            else:
-                # Create a new CustomData object if it doesn't exist
-                label = Label.objects.get_or_create(label=label_name)[0]
-                CustomData.objects.create(
-                    label=label,
-                    text1=form.cleaned_data['text1'],
-                    text2=form.cleaned_data['text2'],
-                    message=form.cleaned_data['message'],
-                    audio1=request.FILES.get('audio1', None),
-                    audio2=request.FILES.get('audio2', None)
-                )
-            # Return a JSON response indicating success
-            return JsonResponse({'success': True})
-        else:
-            # Return a JSON response indicating form errors
-            return JsonResponse({'success': False, 'errors': form.errors})
-    else:
-        # Prepare initial form data
-        initial_data = {
-            'text1': custom_data.text1 if custom_data else '',
-            'text2': custom_data.text2 if custom_data else '',
-            'message': custom_data.message if custom_data else ''
-        }
-        form = CustomForm(initial=initial_data)
-
-    return render(request, 'courses/detailedCourse/custom_page.html', {
-        'form': form,
-        'initial_text1': initial_data['text1'],
-        'initial_text2': initial_data['text2'],
-        'initial_message': initial_data['message']
-    })
+    latest_CustomData_list = CustomData.objects.all()
+    template = loader.get_template("courses/detailedCourse/custom_page.html")
+    context = {
+        "latest_CustomData_list": latest_CustomData_list,
+    }
+    return HttpResponse(template.render(context, request))
